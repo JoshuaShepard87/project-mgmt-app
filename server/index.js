7d9de522d5336c799ca7dd1e162db49198260e9f
Joshua <jburden@aimcodeschool.org>
2022-10-24 00:37:58 -0500

    added ignore

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 5000;
 const app = express();
 const schema = require('./schema/schema');
 
+app.get('/', (req, res) => {
+    res.status(200).send(`Server is running on port ${port}`);
+});
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development'
